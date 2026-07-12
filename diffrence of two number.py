29b560a65dbc678498d67150897ee60162c7178f
Roushan kumar <roussshan@gmail.com>
2026-07-12 23:29:12 +0530

    Create diffrence of two number.py

---
+++ diffrence of two number.py
@@ -0,0 +1,9 @@
+# Taking Two no. from user
+
+num1 = int (input("Enter the 1st number:"))
+num2 = int (input("Enter the 2nd number:"))
+diff = num1 - num2
+
+# The difference is
+
+print ("The difference is :", diff )
