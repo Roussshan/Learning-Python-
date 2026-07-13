f8204a8fd7cf7be85b16b82f5e540d4b561b3805
Roushan kumar <roussshan@gmail.com>
2026-07-13 22:14:35 +0530

    create simple_intrest.py

---
+++ simple_intrest.py
@@ -0,0 +1,12 @@
+# Simple Interest formula: (P * R * T) / 100
+
+# Taking input from the user
+p = float(input("Enter Principal Amount: "))
+r = float(input("Enter Rate of Interest: "))
+t = float(input("Enter Time in Years: "))
+
+# Calculating Simple Interest
+si = (p * r * t) / 100
+
+# Printing the result
+print("Simple Interest is:", si)
